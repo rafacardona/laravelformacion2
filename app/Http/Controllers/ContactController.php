d0da9b54f780c47de6a73202ca1973ea94d2cd68
root <rafacardonaf@gmail.com>
2023-04-28 09:49:54 +0200

    Validando formulario de contacto

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -15,10 +15,20 @@ class ContactController extends Controller
     }
 
     /*
-     * Funcion para cuando se llame al controlador por el metodo send(), de haber enviado el formulario de contacto
+     * Funcion para cuando se llame al controlador por el metodo send(), de haber enviado el formulario de contacto.
+     * Inyenctandole Request podemos validar lo que recibimos de manera sencilla
      */
-    public function send(){
+    public function send(Request $request){
 
+        /*
+         * Con el metodo validate, que recibe como argumentos la request y un arreglo de las reglas de lo que esperamos
+         */
+        $this->validate($request, [
+            "subject" => "required|string|min:5|max:100",
+            "message" => "required|string|min:20|max:300"
+        ]);
+        //de esta manera se puede ver lo que trae la request
+        //dd($request->input());
     }
 
 }
